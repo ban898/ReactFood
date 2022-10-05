2a3dd350d477da4ea2949d36c078e5ec1bf2164c
ban898 <davidface2@gmail.com>
2022-10-05 16:15:26 +0300

    Adeed modal

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import HeaderCartButoon from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButoon />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A Table of food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
